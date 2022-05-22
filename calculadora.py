b4b2958965fa9f891dfd66d862990fe984005959
JesusPreciado <a19311022@uthermosillo.edu.mx>
2022-05-21 21:48:36 -0700

    Se agrego la funciones de multiplicacion y division a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -21,8 +21,21 @@ def resta(n1,n2):
     r=n1-n2
     print("" + str(r))
     
+def mult(n1,n2):
+    print("Mutiplicacion de 2 numeros ")
+    print("La mutiplicacion de " + str(n1) + " * " + str(n2) + " es igual a: ")
+    r=n1*n2
+    print("" + str(r))
     
-# Arranque
-    
+def div(n1,n2):
+    print("Divicion de 2 numeros ")
+    print("La divicion de " + str(n1) + " / " + str(n2) + " es igual a: ")
+    r=n1/n2
+    print("" + str(r))
+           
+# Arranque  
+
 suma(1,2)
 resta(1,2)
+mult(1,2)
+div(1,2)
